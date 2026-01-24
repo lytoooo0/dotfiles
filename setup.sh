b47afb317136bf45a5f9f4d0c09ba1b5395f4fca
lytoooo0 <lytoooo0@icloud.com>
2026-01-24 16:12:55 +0800

    feat: add waybar config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/nvim ~/.config/nvim
 ln -sf ~/dotfiles/zed ~/.config/zed
 ln -sf ~/dotfiles/foot ~/.config/foot
 ln -sf ~/dotfiles/sway ~/.config/sway
+ln -sf ~/dotfiles/waybar ~/.config/waybar
 
 echo "Dotfiles installed!"
 
